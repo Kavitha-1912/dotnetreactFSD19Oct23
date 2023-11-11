c91ef005c4389cdf1508015b62142bb0057958e5
unknown <kavitham1219@gmail.com>
2023-11-11 19:48:46 +0530

    Day 7

---
+++ Day 7/Day 7/SQLQuery1.sql
@@ -0,0 +1,204 @@
+use pubs
+
+select * from publishers
+select * from titles
+
+--select publisher name and the title name
+select pub_name 'Publisher Name', title 'Book Name' from publishers join titles
+on publishers.pub_id = titles.pub_id
+
+select pub_name 'Publisher Name', title 'Book Name' 
+from publishers p join titles t
+on p.pub_id = t.pub_id
+
+select p.pub_id 'Publisher Id', pub_name 'Publisher Name', title 'Book Name' 
+from publishers p join titles t
+on p.pub_id = t.pub_id
+order by [Publisher Name]
+
+--outer join
+select pub_name 'Publisher Name', title 'Book Name' 
+from publishers left outer join titles
+on publishers.pub_id = titles.pub_id
+
+select t.title Book_Name, s.qty Quantity_Sold
+from sales s right outer join titles t
+on s.title_id = t.title_id
+
+--Publisher name and the count of book published
+select pub_name 'Publisher Name',count(t.pub_id) 'Number of books Published'
+from publishers p join titles t
+on p.pub_id=t.pub_id
+group by pub_name
+order by [publisher Name]
+
+--print the publisher name and employee name
+select * from employee
+
+select pub_name as 'publisher name',concat(fname,lname) as 'employee name'
+from publishers p inner join employee e
+on p.pub_id=e.pub_id
+
+--Print the book name and author name
+select title 'Book Name',concat(au_fname,' ',au_lname) 'Author Name'
+from titles t join titleauthor ta
+on t.title_id=ta.title_id
+join authors a on ta.au_id=a.au_id
+
+--Select the publisher name, title name and the sold quantity
+select * from sales
+select * from titles
+select * from publishers
+
+select pub_name 'Publisher_Name',title 'title_name',qty 'Sold_qty'
+from publishers p join titles t
+on p.pub_id = t.pub_id
+join sales s on t.title_id = s.title_id
+
+--cross join
+select * from sales cross join employee
+
+create table tbl1
+(f1 int,
+f2 varchar(20))
+
+insert into tbl1 values(1,'abc'),(2,'efg'),(3,'klj'),(4,'jjj')
+--data injection
+select * from tbl1 where f1=1;delete from tbl1;
+
+--creating procedure
+create procedure proc_First
+as
+begin
+	select *from authors
+end
+
+execute proc_First
+
+create proc proc_InsertSample(@f1 int,@f2 varchar(20))
+as
+begin
+	insert into tbl1 values(@f1,@f2)
+end
+
+exec proc_InsertSample 5,'GYH'
+exec proc_InsertSample 5,'GYH;delete from tbl1'
+
+alter proc proc_InsertSample(@f2 varchar(20))
+as
+begin
+    select * from tbl1 where f2 = @f2
+end
+
+exec proc_InsertSample '1;delete from tbl1'
+
+--create a proc that will take a author name 
+--and fetch the total sales done on his books
+
+create proc proc_GetTotalSaleAmount(@authorName varchar(20),@saleamount float out)
+as
+begin
+   declare
+    @saleamt float
+	set @saleamt = (select sum(s.qty) * sum(t.price) from sales s join titles t 
+						on s.title_id=t.title_id
+						where t.title_id in
+						(select title_id from titleauthor where au_id= 
+						(select au_id from authors where au_fname = @authorName)))
+	set @saleamount = @saleamt
+end
+
+declare @amt float
+begin
+exec proc_GetTotalSaleAmount 'Cheryl',@amt out
+print @amt
+end
+
+--create a procedure that will take the publisher name and give back the total sale 
+--for the books published
+
+select *from publishers
+select *from sales
+
+create proc proc_GetTotalSale(@publisherName varchar(20),@totalsale float out)
+as
+begin
+   declare
+    @sale float
+	set @sale = (select sum(s.qty) from sales s 
+	            join titles t on s.title_id=t.title_id 
+	            where t.pub_id in(select pub_id from publishers where pub_name=@publisherName))
+	set @totalsale= @sale
+end
+ 
+declare @sales int
+begin
+exec proc_GetTotalSale 'New Moon Books',@sales out
+select @sales 'Total Sales'
+end
+
+
+
+
+--funtion
+
+select * from authors
+
+create function fn_CalculateTax(@price float)
+returns float
+as
+begin
+    declare @totalPrice float
+	set @totalPrice = @price +(@price*12.36/100)
+	return round(@totalPrice,2)
+end
+
+select title,price,dbo.fn_Calculatetax(price)'Nett.Price'
+from titles
+
+--create a function that will take the author's first name and last name and 
+--give back the full name separated by space
+
+select *from authors
+
+create function fn_AuthorName(@au_fname char(20), @au_lname char(20))
+returns varchar(50)
+as
+begin
+    declare @Author_Name varchar(50)
+    set @Author_Name = @au_fname + ' ' + @au_lname
+    return @Author_Name
+end
+
+select au_fname, au_lname, dbo.fn_AuthorName(au_fname, au_lname) 'Full_Name'
+from authors;
+
+--view
+
+create view vwPublisher
+as
+	select pub_id 'Publisher Id',pub_name 'Publisher Name' from publishers
+select * from vwPublisher
+
+create view vwInvoice
+as
+ select t.title 'Book Name', sum(s.qty)*sum(t.price) 'Sale Amount' from sales s join titles t
+                    on s.title_id=t.title_id
+					group by t.title 
+
+select * from vwInvoice
+
+create index idxSample
+on tbl1(f1)
+
+sp_help tbl1
+
+create trigger trg_InsertTbl1
+on tbl1
+for insert
+as
+begin
+   print 'Hello'
+end
+
+insert into tbl1 values(101,'UUU')
