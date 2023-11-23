2872ae7db241a7c3bfebe2c602e7487a90be1c6f
Kavitha-1912 <kavitham1219@gmail.com>
2023-11-23 11:26:32 +0530

    Day 20

---
+++ Day 20/first-app/src/Components/Second.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import './Second.css';
+
+function Second(){
+    var second={}
+    return(
+        <div className='second'>
+            <h1>Second Component</h1>
+        </div>
+    );  
+}
+
+export default Second;
